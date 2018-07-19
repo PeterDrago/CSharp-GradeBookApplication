e3626dcb67e2c2d0ac2d1bcab4a9171f6385b9e1
PeterDrago <41273671+PeterDrago@users.noreply.github.com>
2018-07-19 18:29:41 -0500

    Creating Ranked Grading System

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -11,6 +12,40 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students");
+            }
+            List<double> grades = new List<double>();
+            foreach (var student in Students)
+            {
+                grades.Add(student.AverageGrade);
+            }
+            grades.Sort((a, b) => -1* a.CompareTo(b));
+            int threshold =(int)(Students.Count * 0.2);
+            if (grades[threshold - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            if (grades[threshold * 2 -1] <= averageGrade)
+            {
+                return 'B';
+            }
+            if (grades[threshold * 3 - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            if (grades[threshold * 4 -1] <= averageGrade)
+            {
+                return 'D';
+            }
+            return 'F';
+        }
     }
-    
 }
+    
+  
+
