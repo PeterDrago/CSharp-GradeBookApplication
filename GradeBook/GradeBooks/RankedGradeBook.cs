7939ecf22d585772c42d0638cddcbc600cf7928f
PeterDrago <41273671+PeterDrago@users.noreply.github.com>
2018-07-19 19:06:12 -0500

    Took away calculateStudentStatistics Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -56,17 +56,6 @@ namespace GradeBook.GradeBooks
             }
             return;
         }
-        public override void CalculateStudentStatistics(string name)
-        {
-            if (Students.Count < 5)
-            {
-                Console.WriteLine("Ranked-grading requires at least 5 students with grades in order to properly calculate a studen'ts overall grade.");
-            }
-            else
-            {
-                return base.CalculateStudentStatistics();
-            }
-        }
     }
 }
     
