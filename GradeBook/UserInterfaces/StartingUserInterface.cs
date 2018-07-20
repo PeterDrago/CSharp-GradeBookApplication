d8813de8db2595451796a6ef5c7037ef7f8f2b8b
PeterDrago <41273671+PeterDrago@users.noreply.github.com>
2018-07-19 21:19:26 -0500

    Updated the StartingUserInterface

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,40 +41,46 @@ namespace GradeBook.UserInterfaces
             var name = parts[1];
             var type = parts[2];
             BaseGradeBook gradeBook;
-            if (type == "standard");
+            if (type == "standard") 
             {
                 gradeBook = new StandardGradeBook(name);
+            }
             
             else if (type == "ranked")
             {
                 gradeBook = new RankedGradeBook(name);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
+                return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
         public static void LoadCommand(string command)
-        {
-            var parts = command.Split(' ');
-            if (parts.Length != 2)
             {
-                Console.WriteLine("Command not valid, Load requires.");
-                return;
+                var parts = command.Split(' ');
+                if (parts.Length != 2)
+                {
+                    Console.WriteLine("Command not valid, Load requires a name.");
+                    return;
+                }
+                var name = parts[1];
+                var gradeBook = BaseGradeBook.Load(name);
+
+                if (gradeBook == null)
+                    return;
+
+                GradeBookUserInterface.CommandLoop(gradeBook);
             }
-            var name = parts[1];
-            var gradeBook = BaseGradeBook.Load(name);
 
-            if (gradeBook == null)
-                return;
-
-            GradeBookUserInterface.CommandLoop(gradeBook);
-        }
-
-        public static void HelpCommand()
+            public static void HelpCommand()
         {
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook.");
+            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
